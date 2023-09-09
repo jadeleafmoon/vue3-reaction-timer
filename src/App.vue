ecc3b26f47b9c60b64d87c86ed92bdaff8d666ff
jadeleafmoon <jadeleafmoon@gmail.com>
2023-09-08 21:39:16 -0400

    feat: add Results component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <h1>Hi!</h1>
+  <h1>Reaction Game</h1>
   <button @click="start" :disabled="isPlaying">Play</button>
   <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
-  <p v-if="showResults"> Reaction time: {{ score }} ms</p>
+  <Results v-if="showResults" :score="score"></Results>
 </template>
 
 <script>
 import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 
 export default {
   name: 'App',
-  components: { Block },
+  components: { Block, Results },
   data() {
     return {
       isPlaying: false,
---
+++ src/components/Results.vue
@@ -0,0 +1,11 @@
+<template>
+  <h3>Reaction time: {{ score }} ms</h3>
+</template>
+
+<script>
+
+export default {
+  props: ['score']
+}
+
+</script>
